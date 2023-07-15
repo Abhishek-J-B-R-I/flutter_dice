dd033052dcfcee345ec726461b9733f031a51b2d
Abhishek <abhishekmrjbr@outlook.com>
2023-07-15 12:23:37 +0530

    staring page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,23 @@
 import 'package:flutter/material.dart';
-//
-//
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Center(child: Text('Dicee ')),
+        backgroundColor: Colors.red,
+      ),
+      body: DicePage(),
+    ),
+  ));
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
